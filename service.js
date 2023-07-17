65414b3494e8d99bc79a8d07f117cef4b990e331
monthrie <wmontgomery@gmail.com>
2023-07-17 23:18:44 +0100

    fear commit some stuff is working and want to keep it

--- service.js
+++ service.js
@@ -21,9 +21,7 @@ MDS.init(function(msg) {
     
         // Handle the different message types
         handleMessage(jsonData);
-    });
-
-              
+ 
 
 //handle different incoming messages
 function handleMessage(jsonData) {
@@ -45,21 +43,21 @@ function handleAddAddress(jsonData) {
     const name = jsonData.data.name;
     const max = jsonData.data.max;
 
-    MDS.sql(`INSERT INTO AddressBook (name, max) VALUES ('${name}', '${max}')`, function(response) {
-        if (response.status) {
-            MDS.log('New entry added to AddressBook');
-            
-            // Retrieve and log the just-inserted data
-            MDS.sql(`SELECT * FROM AddressBook WHERE name = '${name}' AND max = '${max}'`, function(response) {
-                if (response.status && response.rows.length > 0) {
-                    MDS.log('Just-inserted data: ' + JSON.stringify(response.rows[0]));
+    MDS.log('Adding new address: ' + name + ', ' + max); 
+
+            // If the entry doesn't exist, add it
+            MDS.sql(`INSERT INTO AddressBook (name, max) VALUES ('${name}', '${max}')`, function(response) {
+                if (response.status) {
+                    MDS.log('New entry added to AddressBook:');
+                } else {
+                    MDS.log('There was an error adding the new entry: ' + response.error);
                 }
             });
-        } else {
-            MDS.log('There was an error adding the new entry: ' + response.error);
-        }
-    });
+        
+    
+
 }
+
 
 function handleTableRequest(jsonData) {
     const contact = jsonData.data.contact;
@@ -100,20 +98,10 @@ function sendMessage(message, address, callback) {
         }
     });
 }
-    // run the Maxima command
-    MDS.cmd(maxCmd, function(response) {
-        if (response.status) {
-            MDS.log('Message sent successfully');
-            // Log the message contents
-            MDS.log('Message contents: ' + dataStr);
-        } else {
-            MDS.log('There was an error sending the message: ' + response.error);
-        }
-    });
 
-})
+});
+});
 
 
 // the command that gets us there:
 // maxima action:send to:MAX#0x30819F300D06092A864886F70D010101050003818D00308189028181009BB7465C454425291EBC2A851A4852F8C1B02F7A173A15780B304E2DCA663CC69AF15CA39D21914F5C1C4D20BE1066A29446F1B6AC8BC7FE1AE466D7E672C9BFAB64BA35BEE30ED8217BDB95959EA1B4410C70EF348051642876A8E99138AFCF5933E6DB3DB3ADBB3D418DBFFF30675D8BBB1A534DC5EE03740801579A73A0D10203010001#MxG18HGG6FJ038614Y8CW46US6G20810K0070CD00Z83282G60G1C0ANS2ENGJEFBYJM2SCQFR3U3KBJNP1WS9B0KG1Z2QG5T68S6N2C15B2FD7WHV5VYCKBDW943QZJ9MCZ03ESQ0TDR86PEGUFRSGEJBANN91TY2RVPQTVQSUP26TNR399UE9PPJNS75HJFTM4DG2NZRUDWP06VQHHVQSGT9ZFV0SCZBZDY0A9BK96R7M4Q483GN2T04P30GM5C10608005FHRRH4@78.141.238.36:9001 application:dmaxab data:{"type":"ADD_ADDRESS","data":{"name":"barry","max":"666"}}
-
